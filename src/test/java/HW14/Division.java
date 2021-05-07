84d9a6aff4b8a029f98191257c537701eca6f6e8
Svetlana <svetlana.andreeva@abv.bg>
2021-05-07 14:30:06 +0300

    Test

---
+++ src/test/java/HW14/Division.java
@@ -0,0 +1,23 @@
+package HW14;
+
+import org.junit.Test;
+
+public class Division extends Calculator {
+    @Test
+    public void divisionTest1() {
+
+        System.out.println(divide(12, 3));
+    }
+
+    @Test
+    public void divisionTest2() {
+        System.out.println(divide(10, 2));
+    }
+
+    @Test
+    public void divisionTest3() {
+        System.out.println(divide(15, 3));
+    }
+
+
+}
