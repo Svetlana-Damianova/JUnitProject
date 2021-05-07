17b1fa57114c59f0d754fedac38da8f527e3b7fe
Svetlana <svetlana.andreeva@abv.bg>
2021-05-07 13:54:39 +0300

    test

---
+++ src/test/java/HW14/Calculator.java
@@ -0,0 +1,26 @@
+package HW14;
+
+import org.openqa.selenium.InvalidArgumentException;
+
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        if (a == 0 || b == 0) {
+            throw new InvalidArgumentException("Invalid operation");
+        }
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
