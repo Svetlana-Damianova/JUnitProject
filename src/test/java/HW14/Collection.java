8d1e94d3fd07c4ef81d2d7d6cb29c9752b2ebe96
Svetlana <svetlana.andreeva@abv.bg>
2021-05-07 14:29:51 +0300

    Test

---
+++ src/test/java/HW14/Collection.java
@@ -0,0 +1,24 @@
+package HW14;
+
+import org.junit.Test;
+
+public class Collection extends Calculator {
+
+    @Test
+    public void collectionTest1() {
+
+        System.out.println(sum(5, 5));
+    }
+
+    @Test
+    public void collectionTest2() {
+
+        System.out.println(sum(7, 7));
+    }
+
+    @Test
+    public void collectionTest3() {
+
+        System.out.println(sum(9, 3));
+    }
+}
